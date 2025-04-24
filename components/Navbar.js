595538e61007516ae510acf3f3bf5550233cc765
Ishi Rastogi <ishi.rastogi@icloud.com>
2025-04-24 11:58:27 +0530

    added changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 import styles from '../styles/navbar.module.css';
+const PRODUCT_BASE_URL = "https://repo2-ecommerce-engine.vercel.app/products/";
+
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>Urban Threads</div>
       <div className={styles.links}>
-        <Link href="http://localhost:3001/products/tshirt1">T-Shirts</Link>
-        <Link href="http://localhost:3001/products/hoodie1">Hoodies</Link>
-        <Link href="http://localhost:3001/products/cap1">Baseball Caps</Link>
+      <Link href={`${PRODUCT_BASE_URL}tshirt1`}>T-Shirts</Link>
+<Link href={`${PRODUCT_BASE_URL}hoodie1`}>Hoodies</Link>
+<Link href={`${PRODUCT_BASE_URL}cap1`}>Baseball Caps</Link>
+
       </div>
     </nav>
   );
