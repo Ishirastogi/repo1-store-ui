90d8bf790299541c5dc3fb26bdbb1d5095fd291e
Ishi Rastogi <ishi.rastogi@icloud.com>
2025-04-24 11:48:38 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import styles from '../styles/home.module.css';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
